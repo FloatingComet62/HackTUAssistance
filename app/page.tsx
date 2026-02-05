f0ce8373f576a328d96aeb0f2af0f42d57e855b9
Aargh Rai <shubh2007rai@gmail.com>
2026-02-05 14:12:34 +0530

    small centering

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black">
-        <div className="text-4xl font-bold">HackTU Assistance Portal</div>
+        <div className="text-4xl font-bold text-center">HackTU Assistance Portal</div>
         <form className="flex flex-col gap-4 items-center w-1/2" onSubmit={(e) => {
           e.preventDefault();
           DebouncedSendRequest(table, request);
